938300d643bc27a8daca451f9477054f33c4d297
Taras_Vanko <taras.vanko@deepinspire.com>
2024-05-18 20:11:00 +0300

    added footer and latest components

--- src/app/home/components/home/home.component.ts
+++ src/app/home/components/home/home.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { HomeLatestComponent } from '../home-latest/home-latest.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [HomeLatestComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
